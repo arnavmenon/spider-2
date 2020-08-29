36e851418cba24ca2a35b6ec81c3fc611933ff13
Arnav Menon <63289642+arnavmenon@users.noreply.github.com>
2020-08-30 01:36:19 +0530

    Improved Interface

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
-const lb=document.getElementById('left-btn');
-const rb=document.getElementById('right-btn');
-const qtext=document.querySelector('.qtext');
-const qopts=document.querySelector('.options');
 
-var options = document.querySelectorAll(".option");
+var qtext,qopts,options,lb,rb,sub;
 
 var qnum,quizStatus=0;
 
 
+//indicating answers
 function resetColors(){
     options.forEach(option => {
        option.style.backgroundColor="white";
@@ -32,78 +29,148 @@ function indicateAnswer(){
 }
 
 //start quiz 
-document.getElementById("start-btn").addEventListener('click',()=>{
+
+function openQuiz(){
     document.getElementById("start-window").style.display="none";
+
+    let leftspan=document.createElement('span');
+    let quizBox=document.createElement('span');
+    let rightspan=document.createElement('span');
+    let submit=document.createElement('button');
+
+
+    leftspan.innerHTML=`<button id="left-btn">&lAarr;</button>`;leftspan.className='left-btn-span';
+    rightspan.innerHTML=`<button id="right-btn">&rAarr;</button>`;rightspan.className='right-btn-span';
+    quizBox.id='questions';
+    submit.id='submit';submit.innerHTML='SUBMIT';
+    quizBox.innerHTML=`
+                            <div class="qtext"></div>
+
+                            <div class="options">
+                                <div id="opt1" class="option"></div>
+                                <div id="opt2" class="option"></div>
+                                <div id="opt3" class="option"></div>
+                                <div id="opt4" class="option"></div>
+
+                            </div>`;
+
+    document.querySelector(".main-container").appendChild(leftspan);
+    document.querySelector(".main-container").appendChild(quizBox);
+    document.querySelector(".main-container").appendChild(rightspan);
+    document.querySelector("#sidenav").appendChild(submit);
+
+
+
+    qtext=document.querySelector('.qtext');
+    qopts=document.querySelector('.options');
+    options=document.querySelectorAll(".option");
+    lb=document.getElementById('left-btn');
+    rb=document.getElementById('right-btn');
+    sub=document.getElementById('submit');
+
+    activateListeners();
+}
+
+
+document.getElementById("start-btn").addEventListener('click',()=>{
+
+    openQuiz();
     quizStatus=1;
     qnum=1;
+
     qtext.innerHTML=data[qnum-1].question;
     for(let j=1;j<=4;j++)
         document.getElementById(`opt${j}`).innerHTML=data[qnum-1].options[j-1];
 
 });
 
-//nav bw questions
-rb.addEventListener('click',()=>{
-    if(qnum<10&&qnum>=1){
 
-        qnum++;
-        indicateAnswer();
-        qtext.innerHTML=data[qnum-1].question;
-        for(let j=1;j<=4;j++)
-            document.getElementById(`opt${j}`).innerHTML=data[qnum-1].options[j-1];
 
-    }
-})
 
-lb.addEventListener('click',()=>{
-    if(qnum<=10&&qnum>1){
-        qnum--;
-        indicateAnswer();
-        qtext.innerHTML=data[qnum-1].question;
-        for(let j=1;j<=4;j++)
-            document.getElementById(`opt${j}`).innerHTML=data[qnum-1].options[j-1];
+//listeners for lb,rb,answering option,submit
 
-    }
-})
-
-//answering question
-
-options.forEach(option => {
-
+function activateListeners(){
     
-    option.addEventListener('click',()=>{
+    options.forEach(option => {
 
-        if(data[qnum-1].answered==0){
+            
+            option.addEventListener('click',()=>{
 
-            data[qnum-1].answered=1;
-            let choice=option.id;
-            data[qnum-1].clickedOpt=choice;
-            if(choice==data[qnum-1].answer){         
-                data[qnum-1].correct=1;
+                if(data[qnum-1].answered==0){
+
+                    data[qnum-1].answered=1;
+                    let choice=option.id;
+                    data[qnum-1].clickedOpt=choice;
+                    if(choice==data[qnum-1].answer){         
+                        data[qnum-1].correct=1;
+                    }
+
+                    else{
+                        data[qnum-1].correct=-1;
+                    }
+
+                    indicateAnswer();
+                }
+            })
+
+
+        });
+
+
+        rb.addEventListener('click',()=>{
+            if(qnum<10&&qnum>=1){
+        
+                qnum++;
+                indicateAnswer();
+                qtext.innerHTML=data[qnum-1].question;
+                for(let j=1;j<=4;j++)
+                    document.getElementById(`opt${j}`).innerHTML=data[qnum-1].options[j-1];
+        
             }
-
-            else{
-                data[qnum-1].correct=-1;
+        })
+        
+        lb.addEventListener('click',()=>{
+            if(qnum<=10&&qnum>1){
+                qnum--;
+                indicateAnswer();
+                qtext.innerHTML=data[qnum-1].question;
+                for(let j=1;j<=4;j++)
+                    document.getElementById(`opt${j}`).innerHTML=data[qnum-1].options[j-1];
+        
             }
+        })
 
-            indicateAnswer();
-        }
-    })
+        sub.addEventListener('click',()=>{
+            findScore();
+        })
+    
+}
 
 
-});
+//end quiz
 
+function endQuiz(){
 
-//calc score
+    document.querySelector(".left-btn-span").style.display="none";
+    document.querySelector(".right-btn-span").style.display="none";
+    document.querySelector("#questions").style.display="none";
+
+    let scorebox=document.createElement('div');
+    scorebox.id='score';
+    document.querySelector(".main-container").appendChild(scorebox);
+
+}
 function findScore(){
 
-    if(quizStatus==1){    
+    if(quizStatus==1){ 
+
         quizStatus=0;
+        endQuiz();
         var score=0;
         data.forEach(item=>{
             if(item.correct==1) score++;
         })
-        document.getElementById("score").innerHTML+=score;
+        document.getElementById("score").textContent+=score;
     }
 }
   
