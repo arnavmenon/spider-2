b43f3f09ebb313038da4a807c3edc951be34ba1c
Arnav Menon <63289642+arnavmenon@users.noreply.github.com>
2020-08-31 19:01:28 +0530

    Added hacker features

--- js/script.js
+++ js/script.js
@@ -1,8 +1,43 @@
 
-var qtext,qopts,options,lb,rb,sub,qnav;
+var qtext,qopts,options,lb,rb,sub,qnav,name,userscore;
+const timelimit=180;
+var timeleft=timelimit,x;
 var sidenav=document.querySelector("#sidenav");
 var qnum,answered=0,quizStatus=0;
 
+
+//initialize localstorage items
+
+var scoresArray=[{name:'',score:0,datetime:''},{name:'',score:0,datetime:''},{name:'',score:0,datetime:''},{name:'',score:0,datetime:''},{name:'',score:0,datetime:''}];
+
+if(localStorage.getItem('scores')==null) localStorage.setItem('scores',JSON.stringify(scoresArray))
+else {
+    scoresArray=JSON.parse(localStorage.getItem('scores'));
+    console.log(scoresArray);
+}
+
+
+
+//randomize question order
+var shuffle=function(){
+    var temp=data;
+
+    var currentIndex = temp.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = temp[currentIndex];
+    temp[currentIndex] = temp[randomIndex];
+    temp[randomIndex] = temporaryValue;
+    }
+
+    data=temp;
+}
+
+shuffle();
 
 //indicating answers
 function resetColors(){
@@ -31,6 +66,7 @@ function indicateAnswer(){
 
 function openQuiz(){
     document.getElementById("start-window").style.display="none";
+    document.querySelector(".nameentry").remove();
 
     let leftspan=document.createElement('span');
     let quizBox=document.createElement('span');
@@ -52,13 +88,6 @@ function openQuiz(){
                                 <div id="opt4" class="option"></div>
 
                             </div>`;
- /*    navbox.innerHTML=`  
-                        <div id="row1">
-                        <span id="nav1">1</span><span id="nav2">2</span><span id="nav3">3</span><span id="nav4">4</span><span id="nav5">5</span>
-                        </div> 
-                        <div id="row2">
-                        <span id="nav6">6</span><span id="nav7">7</span><span id="nav8">8</span><span id="nav9">9</span><span id="nav10">10</span>
-                        </div>` */
         
     navbox.innerHTML=`  
                         <div id="row1">
@@ -66,7 +95,8 @@ function openQuiz(){
                         </div> 
                         <div id="row2">
                         <span class="navbox">6</span><span class="navbox">7</span><span class="navbox">8</span><span class="navbox">9</span><span class="navbox">10</span>
-                        </div>`
+                        </div>
+                        <div id="timer"></div>`
 
     document.querySelector(".main-container").appendChild(leftspan);
     document.querySelector(".main-container").appendChild(quizBox);
@@ -83,21 +113,29 @@ function openQuiz(){
     rb=document.getElementById('right-btn');
     sub=document.getElementById('submit');
     qnav=document.querySelectorAll('.navbox');
-    console.log(qnav);
+    //console.log(qnav);
 
     activateListeners();
+    startTimer();
 }
 
 
 document.getElementById("start-btn").addEventListener('click',()=>{
 
+    name=document.querySelector(".name").value;
+    if(name=="") alert('Please enter your name in the field');
+    else {
+
     openQuiz();
+    
+    localStorage.setItem('name',name);
     quizStatus=1;
     qnum=1;
 
     qtext.innerHTML=data[qnum-1].question;
     for(let j=1;j<=4;j++)
         document.getElementById(`opt${j}`).innerHTML=data[qnum-1].options[j-1];
+    }
 
 });
 
@@ -181,6 +219,29 @@ function activateListeners(){
 }
 
 
+//start the timer
+
+function startTimer(){
+     x = setInterval(function() {
+
+        timeleft--;
+
+        var minutes=Math.floor(timeleft/60);
+        var seconds=timeleft%60;
+        minutes=String("0" + minutes).slice(-2);
+        seconds=String("0" + seconds).slice(-2);
+
+        document.getElementById("timer").innerHTML=minutes + ":" + seconds;
+      
+        if (timeleft <= 0) {
+          clearInterval(x);
+          sub.click();
+        }
+      }, 1000);
+}
+
+
+
 //end quiz
 
 function endQuiz(){
@@ -188,12 +249,16 @@ function endQuiz(){
     document.querySelector(".left-btn-span").style.display="none";
     document.querySelector(".right-btn-span").style.display="none";
     document.querySelector("#questions").style.display="none";
+    clearInterval(x);
+    document.getElementById("timer").remove();
+    document.getElementById("navcontainer").remove();
 
     let scorebox=document.createElement('div');
     scorebox.id='scorebox';
-    scorebox.innerHTML=`<span><u>Your Score</u>
+    scorebox.innerHTML=`<span><h3><u>Your Score</u></h3>
                         <span id="score"></span>
                         </span>
+                        <div id="highscores"><h3><u>Highscores</u></h3></div>
                         <button id="restart">Try Again!</button>`
     document.querySelector(".main-container").appendChild(scorebox);
     document.getElementById('restart').addEventListener('click',()=>{
@@ -201,17 +266,54 @@ function endQuiz(){
     })
 
 }
+
+function findDateTime(){
+    var today = new Date();
+    var date = today.getDate()+"-"+(today.getMonth()+1)+"-"+today.getFullYear();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
+    return dateTime;
+}
+
+
+//calculate and display score,highscores
 function findScore(){
 
     if(quizStatus==1){ 
 
         quizStatus=0;
         endQuiz();
-        var score=0;
+        userscore=0,x=1,highscores=document.getElementById("highscores");
+        if(timeleft<=0) timeleft=0;
+
+        var dateTime=findDateTime();
         data.forEach(item=>{
-            if(item.correct==1) score++;
+            if(item.correct==1) userscore++;
         })
-        document.getElementById("score").innerHTML+=score;
+
+        //algo to calc score based on time,corrct to wrong ratio
+
+        userscore=(userscore)*7 + (timeleft/180)*(userscore/10)*30;
+        userscore=(Math.round(userscore * 100) / 100).toFixed(2);
+
+        for(let i=0;i<5;i++){
+            if(parseFloat(userscore)>=parseFloat(scoresArray[i].score)){
+                for(let j=5;j>=i+1;j--)
+                    {scoresArray[j]=scoresArray[j-1];}
+                scoresArray[i]={name:`${localStorage.getItem('name')}`,score:userscore,datetime:`${dateTime}`}
+                scoresArray.splice(5);
+                localStorage.setItem('scores',JSON.stringify(scoresArray));
+                break;
+            }
+        }
+        console.log(scoresArray);
+        document.getElementById("score").innerHTML+=localStorage.getItem('name')+"&nbsp:&nbsp"+userscore;
+        for(let i=0;i<5;i++){
+            if(scoresArray[i].name!=''){
+                highscores.innerHTML+=`<div class"hsrow">${x}. ${scoresArray[i].name} : ${scoresArray[i].score} &nbsp&nbsp <span class="datespan">(${scoresArray[i].datetime}) </span> </div>`;
+                x++;    
+            }
+        }
     }
 }
   
